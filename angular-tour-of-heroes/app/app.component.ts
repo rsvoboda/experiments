0ad133c3551ffcba10a8304f525f82ee0b8dfaec
Rostislav Svoboda <rsvoboda@redhat.com>
2016-12-29 23:14:40 +0100

    angular-tour-of-heroes - part 4

--- angular-tour-of-heroes/app/app.component.ts
+++ angular-tour-of-heroes/app/app.component.ts
@@ -1,20 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-import { HeroDetailComponent } from './hero-detail.component';
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -78,12 +64,28 @@ const HEROES: Hero[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+ providers: [HeroService]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) {
+    //this.heroes = this.heroService.getHeroes();  //keep complex logic out of the constructor
+  }
+
+  getHeroes(): void {
+    // this.heroes = this.heroService.getHeroesDirectly();
+    // this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     console.log("onSelect event - " + hero.name);
--- angular-tour-of-heroes/app/hero.ts
+++ angular-tour-of-heroes/app/hero.ts
@@ -1,8 +1,4 @@
 export class Hero {
   id: number;
   name: string;
-
-  public toString = () : string => {
-        return `Hero (id: ${this.id}, name: ${this.name})`;
-    }
 }
